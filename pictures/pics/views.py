87c98a6e53c8d3bb8cb7775a57ce64dd930b716a
Mary Mbugua <marymbugua.nm@gmail.com>
2018-05-17 10:16:23 +0300

    basic urls and viw function to load index page

--- pictures/pics/views.py
+++ pictures/pics/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 
 # Create your views here.
+
+def landing(request):
+  
+    
+    return render(request,'index.html')
+    
