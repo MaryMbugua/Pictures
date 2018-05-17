1929492f13f86dd5fafb8b9ec67d99f552d36d92
Mary Mbugua <marymbugua.nm@gmail.com>
2018-05-17 11:37:13 +0300

    all templates and links to navigate the website set up

--- pictures/pics/urls.py
+++ pictures/pics/urls.py
@@ -6,5 +6,11 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.landing,name='landing'),
-
+    url(r'^locations/$',views.places,name='places'),
+    url(r'^locations/naivasha$',views.naivasha,name='naivasha'),
+    url(r'^locations/nairobi$',views.nairobi,name='nairobi'),
+    url(r'^locations/kitale$',views.kitale,name='kitale'),
+    url(r'^locations/kisumu$',views.kisumu,name='kisumu'),
+    url(r'^locations/watamu$',views.watamu,name='watamu'),
+    url(r'^locations/nyeri$',views.nyeri,name='nyeri'),
    ]
--- pictures/pics/views.py
+++ pictures/pics/views.py
@@ -7,4 +7,35 @@ def landing(request):
   
     
     return render(request,'index.html')
+def places(request):
+  
+    
+    return render(request,'locs.html')
+
+
+def naivasha(request):
+  
+    
+    return render(request,'vasha.html')
+    
+def kisumu(request):
+  
+    
+    return render(request,'sumu.html')
+def nyeri(request):
+  
+    
+    return render(request,'nyer.html')
+def kitale(request):
+  
+    
+    return render(request,'kit.html')
+def nairobi(request):
+  
+    
+    return render(request,'nai.html')
+def watamu(request):
+  
+    
+    return render(request,'wat.html')
     
